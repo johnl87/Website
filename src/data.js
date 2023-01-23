186026808db5546a36859ab81ea18ccac42c399e
John Le <65139320+johnl87@users.noreply.github.com>
2023-01-22 20:43:17 -0500

    init

---
+++ src/data.js
@@ -0,0 +1,72 @@
+export const projects = [
+  {
+    title: "Python Artifical Intelligence ChatBot",
+    subtitle: "Artifical Intelligence",
+    description:
+      "A chatbot that uses tensorflow and keras to understand user input and generate a response",
+    image: "./project1.gif",
+    //link: "",
+  },
+  {
+    title: "Artifical Intelligence Image Generator by OpenAI",
+    subtitle: "Image Generator",
+    description:
+      "using javscript, html, css, react, and node.js to create an API that uses OpenAI image generator",
+    image: "./project2.gif",
+    //link: "https://github.com",
+  },
+  {
+    title: "Java calculator",
+    subtitle: "JavaFX Simple Calculator",
+    description:
+      "using javaFX to create a simple calulator that will add or subtract two user provided operands",
+    image: "./project3.gif",
+    //link: "https://reactbootcamp.com",
+  },
+  {
+    title: "QR Code",
+    subtitle: "QR generator with Javascript",
+    description:
+      "QR code that can be generated in various dimensions which can also be saved as a image",
+    image: "./project4.gif",
+    //link: "https://reactbootcamp.com",
+  },
+];
+
+export const educations = [
+  {
+    quote:
+      "Focused on topics in human Health, aging, and diseases. Using various software to examine themes related to the science field as well as developing knowledge translation skills.",
+    image: "https://nature.mcmaster.ca/app/uploads/2020/07/Screen-Shot-2020-09-01-at-5.25.17-PM.png",
+    name: "McMaster University",
+    company: "Honours Life Sciences",
+  },
+  {
+    quote:
+      "Focused on theory and practical experience in algorithms, programming languages, system software and tools. Foundation within data structures, assembly language, networking, and software engineering.",
+    image: "https://d31cuupyjbe4e1.cloudfront.net/ADMIN/Algoma%20Logo-ad18f041-5bed-4455-aa9a-d38b65f81505.png",
+    name: "Algoma University",
+    company: "Honours Computer Science",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Python",
+  "Node",
+  "Python",
+  "C++",
+  "Java",
+  "Jira",
+  "Notion",
+  "MatLab",
+  "HTML",
+  "CSS",
+  "Microsoft Excel",
+  "Microsoft Word",
+  "Tableau",
+  "Minitab",
+  "Github",
+  "Postman"
+];
